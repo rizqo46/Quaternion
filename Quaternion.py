92fb6cdb7b39e7fa30e20fe6cb610d8b9ad8e40f
rizqo46 <rizqo1412@gmail.com>
2021-07-23 15:45:43 +0700

    Add init and repr function

---
+++ Quaternion.py
@@ -0,0 +1,16 @@
+class Quaternion(object):
+	"""docstring for Quaternion"""
+	def __init__(self, h, i, j, k):
+		super(Quaternion, self).__init__()
+
+		self.h = h
+		self.i = i
+		self.j = j
+		self.k = k
+		self.value = (self.h, self.i, self.j, self.k)
+
+
+	def __repr__(self):
+		return "Quaternion({})".format(self.value)
+
+		
